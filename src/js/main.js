1956321cba85ba2dbf96516e6300a349a0d33952
Roman <R.BlackNet@gmail.com>
2021-07-18 11:59:46 +0300

    slider fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,27 @@ Swiper.use([ Navigation, Parallax, Autoplay ]);
 
 document.addEventListener('DOMContentLoaded', () => {
   
+    const homeSlider = new Swiper('.home-slider', {
+        speed: 1700,
+        spaceBetween: 100,
+        autoplay: {
+            delay: 6000,
+            pauseOnMouseEnter: true
+        },
+        parallax: true,
+        navigation: {
+            nextEl: '.home-slider__btn-next',
+            prevEl: '.home-slider__btn-prev'
+        }
+    });
+
     const reviewsSlider = new Swiper('.reviews-slider', {
         speed: 1400,
-        slidesPerView: 'auto',
         breakpoints: {
             320: {
                 slidesPerView: 'auto',
-                spaceBetween: 30,
                 centeredSlides: true,
+                // spaceBetween: 30,
             },
             1200: {
                 spaceBetween: 30,
@@ -25,27 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const homeSlider = new Swiper('.home-slider', {
-        speed: 1700,
-        spaceBetween: 100,
-        autoplay: {
-            delay: 6000,
-            pauseOnMouseEnter: true
-          },
-        autoHeight: true,
-        parallax: true,
-        breakpoints: {
-            1100: {
-                autoHeight: false,
-            }
-        },
-        navigation: {
-            nextEl: '.home-slider__btn-next',
-            prevEl: '.home-slider__btn-prev'
-        }
-    });
-
-
     const accordion = () => {
         const aboutTitles = document.querySelectorAll('.about-accordion__item-title');
 
@@ -57,16 +49,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    const removeActiveClass = (elSelector, activeClass) => {
-        const elements = document.querySelectorAll(elSelector);
-
-        elements.forEach(el => {
-            el.classList.remove(activeClass);
-        });
-    };
 
     const tabs = ({ tabSelector, tabActiveClass, contentElSelector, contentActiveClass }) => {
         const tabs = document.querySelectorAll(tabSelector);
+
+        const removeActiveClass = (elSelector, activeClass) => {
+            const elements = document.querySelectorAll(elSelector);
+    
+            elements.forEach(el => {
+                el.classList.remove(activeClass);
+            });
+        };    
 
         const showContent = (tab) => {
             const tabType = tab.getAttribute('data-tab-type');
@@ -89,6 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
             tab.addEventListener('click', () => {
                 removeActiveClass(tabSelector, tabActiveClass);
                 tab.classList.add(tabActiveClass);
+                removeActiveClass()
                 showContent(tab);
             });
         });
@@ -97,12 +91,21 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const toggleMenu = () => {
-        const menuBtn = document.querySelector('.menu-btn');
-        const menuList = document.querySelector('.menu__list');
+        const menu = document.querySelector('.menu');
+        const menuBtn = menu.querySelector('.menu-btn');
 
         menuBtn.addEventListener('click', () => {
             menuBtn.classList.toggle('menu-btn--active');
-            menuList.classList.toggle('menu__list--active');
+            menu.classList.toggle('menu--active');
+        });
+
+        menu.addEventListener('click', (e) => {
+            const target = e.target;
+
+            if (target.classList.contains('menu--active')) {
+                menu.classList.remove('menu--active');
+                menuBtn.classList.remove('menu-btn--active');
+            }
         });
     };
 
@@ -132,36 +135,38 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    const swapCurrentTheme = (btn) => {
-        const images = document.querySelectorAll('img[data-theme-img]');
-        const icons = document.querySelectorAll('img[data-theme-icon]');
-        const theme = btn.getAttribute('data-theme');
-
-        document.body.className = `${theme}`;
-
-        localStorage.setItem('theme', theme);
-
-        const changeSrcPath = (images, directory) => {
-            images.forEach(img => {
-                const filename = img.src.split('/').pop();
-                let path = `img/${theme}/${filename}`;
-
-                if (directory) {
-                    path = `img/${theme + '/' + directory}/${filename}`;
-                }
-
-                img.src = path;
-            });
-        };
-
-        changeSrcPath(images);
-        changeSrcPath(icons, 'icons');
-    };
 
     const changeTheme = () => {
         const currentTheme = localStorage.getItem('theme');
         const themeBtn = document.querySelector('.theme-menu__btn');
         const themeMenu = document.querySelector('.theme-menu');
+
+
+        const swapCurrentTheme = (btn) => {
+            const images = document.querySelectorAll('img[data-theme-img]');
+            const icons = document.querySelectorAll('img[data-theme-icon]');
+            const theme = btn.getAttribute('data-theme');
+    
+            document.body.className = `${theme}`;
+    
+            localStorage.setItem('theme', theme);
+    
+            const changeSrcPath = (images, directory) => {
+                images.forEach(img => {
+                    const filename = img.src.split('/').pop();
+                    let path = `img/${theme}/${filename}`;
+    
+                    if (directory) {
+                        path = `img/${theme + '/' + directory}/${filename}`;
+                    }
+    
+                    img.src = path;
+                });
+            };
+    
+            changeSrcPath(images);
+            changeSrcPath(icons, 'icons');
+        };
 
         if (currentTheme !== null) {
             document.body.className = currentTheme;
